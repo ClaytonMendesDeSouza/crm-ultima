df0ce643fc3672065fc3bcd0db960f98dcc5c57b
Clayton Mendes De Souza <claytonmendes26@hotmail.com>
2023-03-21 20:45:55 -0300

    melhoria de codigo para add boas praticas

--- src/main/java/com/ultimaschool/java/Main.java
+++ src/main/java/com/ultimaschool/java/Main.java
@@ -6,12 +6,16 @@ public class Main {
     public static void main(String[] args) {
 
         //Objeto
-        Cliente clienteF = new Cliente(" 124.455.100.25 "," claytonmendeshotmail.com ",
-                " Ton medeiros or ",  'F');
+        Cliente clienteM = new Cliente("Ton","Mendes","Or","333.222.333.22",
+                "22/02/1998",'M', "claytonmendeshotmail.com","Av.Jequitiba",
+                "3333 - 2222");
+        System.out.println(clienteM.toString());
+
+        Cliente clienteF = new Cliente("Arminia","Mary","Ar","222.333.444.55",
+                "22/02/1989",'F', "Eclipsehotmail.com","Av.Orvalho",
+                "4444 - 5555");
         System.out.println(clienteF.toString());
 
-        Cliente clienteH = new Cliente(" 124.455.100.25 "," claytonmendeshotmail.com ",
-                " Ton medeiros or ",  'M');
-        System.out.println(clienteH.toString());
+
     }
 }
